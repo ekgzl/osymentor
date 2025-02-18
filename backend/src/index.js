c30b5ad743eb15baed1bec65c7939460581a4eba
ekgzl <enesgozuela@gmail.com>
2025-02-18 20:41:30 +0300

    origin vercel eklendi

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,37 +28,35 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5175", // Frontend URL'si
-    credentials: true, // Cookie'leri göndermek için
+    origin: process.env.FRONTEND_URL || "http://localhost:5175",
+    credentials: true,
   })
 );
 
-// Token doğrulama ve cookie oluşturma
 app.post("/api/v1/login", async (req, res) => {
-  const { idToken } = req.body; // Firebase'den gelen JWT token'ı
+  const { idToken } = req.body; // firebase JWT token
 
   if (!idToken) {
     return res.status(400).json({ error: "Token bulunamadı" });
   }
 
   try {
-    // Token'ı doğrula
     const decodedToken = await admin.auth().verifyIdToken(idToken);
-    const expiresIn = 60 * 1000; // 1 dakika
+    const expiresIn = 60 * 1000;
 
-    // Cookie oluştur
+    // cookie oluştur
     res.cookie("authToken", idToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // HTTPS için
+      secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
       maxAge: expiresIn,
     });
     const toUser = {
       username: decodedToken.email.split("@")[0] || "defaultUsername",
       email: decodedToken.email || "defaultEmail",
-      exam: "YKS SAY", // Varsayılan değer
-      avatar: decodedToken.picture || "https://example.com/default-avatar.png", // Varsayılan avatar
-      birthdate: "", // Varsayılan değer
+      exam: "YKS SAY",
+      avatar: decodedToken.picture || "https://example.com/default-avatar.png",
+      birthdate: "",
     };
     res.json({ status: "success", user: toUser });
   } catch (error) {
@@ -71,9 +69,8 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
 
-// Kullanıcı bilgilerini getir
 app.get("/api/v1/user", async (req, res) => {
-  const token = req.cookies.authToken; // Cookie'den token'ı al
+  const token = req.cookies.authToken; // cookie'den token al
 
   if (!token) {
     return res.status(401).json({ error: "Token bulunamadı" });
@@ -95,7 +92,6 @@ app.get("/api/v1/user", async (req, res) => {
   }
 });
 
-// Çıkış yap
 app.post("/api/v1/logout", (req, res) => {
   res.clearCookie("authToken", {
     httpOnly: true,
