c3f05ae30cdb46d0f0295f8efbf9737b4c401fb9
ekgzl <enesgozuela@gmail.com>
2025-02-21 03:34:00 +0300

    samesite degisti

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,6 +30,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
+    methods: "GET,POST,PUT,DELETE",
   })
 );
 
@@ -53,7 +54,7 @@ app.post("/api/v1/login", async (req, res) => {
     res.cookie("authToken", idToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "strict",
       maxAge: expiresIn,
     });
     const toUser = {
