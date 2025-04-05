327ccb0a48d77797a269f7c9b32bdd56a1b47cc8
ekgzl <enesgozuela@gmail.com>
2025-04-05 19:10:08 +0300

    unused import deleted.

--- frontend/src/components/appComponents/timekeeperComponents/Chronometer.tsx
+++ frontend/src/components/appComponents/timekeeperComponents/Chronometer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useStopwatch } from "react-timer-hook";
 import { RootState } from "../../../../app/store";
