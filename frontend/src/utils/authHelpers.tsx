a6fd5b75a341e37cf56a84a40aa04104df2c89f1
ekgzl <enesgozuela@gmail.com>
2025-07-10 11:40:26 +0300

    Update package dependencies and add Toast notification utility

--- frontend/src/utils/authHelpers.tsx
+++ frontend/src/utils/authHelpers.tsx
@@ -7,60 +7,20 @@ import {
 } from "firebase/auth";
 import { auth, googleProvider } from "../config/firebase-config";
 import axios from "axios";
-import Swal from "sweetalert2";
+import { Toast } from "./toastConfig";
 
-const Toast = Swal.mixin({
-  toast: true,
-  position: "top-end",
-  showConfirmButton: false,
-  timer: 1000,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-    toast.onmouseenter = Swal.stopTimer;
-    toast.onmouseleave = Swal.resumeTimer;
-  },
-});
-
-export const handleGoogleRedirect = async (navigate: Function) => {
+export const handleGoogleRedirect = async () => {
   console.log("fonksiyon başı");
   try {
     await signInWithRedirect(auth, googleProvider);
   } catch (error) {
     console.error("Google Redirect Error:", error);
   }
-
-  const result = await getRedirectResult(auth);
-  console.log("dönüş yok");
-  if (result) {
-    console.log("buraya girdim");
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential?.accessToken;
-
-    await axios
-      .post(
-        `${import.meta.env.VITE_API_URL}/api/v1/auth/login`,
-        { idToken: token },
-        { withCredentials: true }
-      )
-      .then(() => {
-        Toast.fire({
-          icon: "success",
-          title: "Giriş başarılı! Uygulamaya aktarılıyorsun..",
-        }).then(() => {
-          console.log("babafireda");
-          navigate("/app");
-        });
-        console.log("Giriş yaptı");
-      })
-      .catch((error) => {
-        console.log("Giriş yapılırken hata oluştu", error);
-      });
-  } else {
-    console.log("result gelmiyor");
-  }
 };
 
-export const handleGoogleLogin = async (navigate: Function): Promise<void> => {
+export const handleGoogleLogin = async (
+  navigate: (path: string) => void
+): Promise<void> => {
   try {
     const result: UserCredential = await signInWithPopup(auth, googleProvider);
     const token: string = await result.user.getIdToken();
@@ -71,13 +31,8 @@ export const handleGoogleLogin = async (navigate: Function): Promise<void> => {
       { withCredentials: true }
     );
 
-    Toast.fire({
-      icon: "success",
-      title: "Giriş başarılı! Uygulamaya aktarılıyorsun..",
-    }).then(() => {
-      console.log("babafireda");
-      navigate("/app");
-    });
+    await Toast("Giriş başarılı! Uygulamaya aktarılıyorsun..", "success");
+    navigate("/app");
   } catch (error) {
     console.error("Google Popup Hatası:", error);
   }
---
+++ frontend/src/utils/toastConfig.tsx
@@ -0,0 +1,18 @@
+import Swal from "sweetalert2";
+
+// union type -> "success" | "error" | "warning" bunlardan biri verilmezse error verir
+export const Toast = (title: string, icon: "success" | "error" | "warning") => {
+  Swal.mixin({
+    toast: true,
+    icon: icon,
+    title: title,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 1400,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
+};
